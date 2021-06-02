67d92b241540f28f014d6b7973220de2972e9d4c
ayumitanaka <tanakayumi13@gmail.com>
2021-06-02 20:11:52 +0000

    added test.js

--- index.js
+++ index.js
@@ -11,43 +11,57 @@ const tasks = [
   { id: 3, name: "Task 3", completed: false },
 ];
 
-// GET
+//GET
 app.get("/api/tasks", (req, res) => {
   res.send(tasks);
 });
 
-// GET BY ID
+//GET (BY ID)
 app.get("/api/tasks/:id", (req, res) => {
   const taskId = req.params.id;
   const task = tasks.find((task) => task.id === +taskId);
-  if (!task) return res.status(404).send("Not Exist");
+  if (!task)
+    return res
+      .status(404)
+      .send("The task with the provided ID does not exist.");
   res.send(task);
 });
 
-// POST
+//POST
 app.post("/api/tasks", (req, res) => {
+  //validation check
   const { error } = utils.validateTask(req.body);
+
   if (error)
-    return res.status(400).send("The name should be more than 3 characters");
+    return res
+      .status(400)
+      .send("The name should be at least 3 characters long");
 
   const task = {
     id: tasks.length + 1,
     name: req.body.name,
     completed: req.body.completed,
   };
+
   tasks.push(task);
   res.status(201).send(task);
 });
 
-// PUT
+//PUT
 app.put("/api/tasks/:id", (req, res) => {
   const taskId = req.params.id;
   const task = tasks.find((task) => task.id === +taskId);
-  if (!task) return res.status(404).send("Not Exist");
+  if (!task)
+    return res
+      .status(404)
+      .send("The task with the provided ID does not exist.");
 
   const { error } = utils.validateTask(req.body);
+
   if (error)
-    return res.status(400).send("The name should be more than 3 characters");
+    return res
+      .status(400)
+      .send("The name should be at least 3 characters long");
 
   task.name = req.body.name;
   task.completed = req.body.completed;
@@ -55,29 +69,38 @@ app.put("/api/tasks/:id", (req, res) => {
   res.send(task);
 });
 
-// PATCH
+//PATCH
 app.patch("/api/tasks/:id", (req, res) => {
   const taskId = req.params.id;
   const task = tasks.find((task) => task.id === +taskId);
-  if (!task) return res.status(404).send("Not Exist");
+  if (!task)
+    return res
+      .status(404)
+      .send("The task with the provided ID does not exist.");
 
   const { error } = utils.validateTask(req.body);
+
   if (error)
-    return res.status(400).send("The name should be more than 3 characters");
+    return res
+      .status(400)
+      .send("The name should be at least 3 characters long");
 
   task.name = req.body.name;
-
   if (req.body.completed) {
     task.completed = req.body.completed;
   }
+
   res.send(task);
 });
 
-// DELETE
+//DELETE
 app.delete("/api/tasks/:id", (req, res) => {
   const taskId = req.params.id;
   const task = tasks.find((task) => task.id === +taskId);
-  if (!task) return res.status(404).send("Not Exist");
+  if (!task)
+    return res
+      .status(404)
+      .send("The task with the provided ID does not exist.");
 
   const index = tasks.indexOf(task);
   tasks.splice(index, 1);
@@ -86,5 +109,5 @@ app.delete("/api/tasks/:id", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 module.exports = app.listen(PORT, () =>
-  console.log(`Server listening on Port: ${PORT}`)
+  console.log(`Server listening on PORT: ${PORT}`)
 );
--- util/task-schema.js
+++ util/task-schema.js
@@ -2,9 +2,7 @@ const Joi = require("joi");
 
 const taskSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  completed: Joi.boolean()
+  completed: Joi.boolean(),
 });
 
-exports.validateTask = (task) => {
-  taskSchema.validate(task);
-};
+exports.validateTask = (task) => taskSchema.validate(task);
